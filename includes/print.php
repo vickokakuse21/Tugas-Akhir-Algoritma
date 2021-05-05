262d1099e2962e09dd229c89a6b9c389d2ea9e22
vicko pratama ibrahim <37886885+vickokakuse21@users.noreply.github.com>
2021-05-05 20:23:01 +0700

    hapus

--- includes/print.php
+++
@@ -1,3 +0,0 @@
-<script language="javascript">
-javascript:print();
-</script>
--- logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-require_once 'config.php';
-
-session_destroy();
-exit("<script>window.location='".$www."';</script>");
-
-?>
